fc5fc678eb49971c7c9ced25a5577c896602b45b
PhatTran-71 <tanphattra44@gmail.com>
2025-11-10 23:38:54 +0800

    update

--- QLRP2/QLRP2/Program.cs
+++ QLRP2/QLRP2/Program.cs
@@ -16,7 +16,7 @@ namespace QLRP2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new qlrp2Form());
         }
     }
 }
