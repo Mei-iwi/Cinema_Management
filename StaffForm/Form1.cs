c59c015e0209fa1606a96208fe421b3c7ec8078b
Mei-IwI <c2005vn@gmail.com>
2025-11-11 14:22:33 +0700

    Add project StaffForm, CustomerForm, MemberTierForm, MovieForm, ShowtimeForm, TicketForm

---
+++ StaffForm/Form1.cs
@@ -0,0 +1,12 @@
+using Common;
+using Cinema_Management;
+namespace StaffForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
