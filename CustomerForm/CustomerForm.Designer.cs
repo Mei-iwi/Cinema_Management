70fc670ca74b9978fe34b3572925159bebfe18d6
PhatTran-71 <tanphattra44@gmail.com>
2025-11-12 20:11:24 +0800

    Update tt

--- CustomerForm/CustomerForm.Designer.cs
+++ CustomerForm/CustomerForm.Designer.cs
@@ -28,18 +28,309 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            txtMaKhachHang = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            txtTen = new TextBox();
+            txtSDT = new TextBox();
+            txtDiaChi = new TextBox();
+            txtEmail = new TextBox();
+            mstNgay = new MaskedTextBox();
+            dgvThongTinKhachHang = new DataGridView();
+            btnThem = new Button();
+            button2 = new Button();
+            btnSua = new Button();
+            btnLoad = new Button();
+            btnDong = new Button();
+            label9 = new Label();
+            label10 = new Label();
+            cboPhai = new ComboBox();
+            cboMaHang = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dgvThongTinKhachHang).BeginInit();
             SuspendLayout();
             // 
-            // Form1
+            // label1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(419, -1);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(481, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Quản Lý Thông Tin Khách Hàng";
+            // 
+            // txtMaKhachHang
+            // 
+            txtMaKhachHang.Location = new Point(227, 121);
+            txtMaKhachHang.Name = "txtMaKhachHang";
+            txtMaKhachHang.Size = new Size(201, 35);
+            txtMaKhachHang.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(23, 129);
+            label2.Name = "label2";
+            label2.Size = new Size(170, 27);
+            label2.TabIndex = 2;
+            label2.Text = "Mã Khách Hàng";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(445, 129);
+            label3.Name = "label3";
+            label3.Size = new Size(106, 27);
+            label3.TabIndex = 3;
+            label3.Text = "Tên Hạng";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(767, 134);
+            label4.Name = "label4";
+            label4.Size = new Size(175, 27);
+            label4.TabIndex = 4;
+            label4.Text = "Tên Khách Hàng";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(1108, 134);
+            label5.Name = "label5";
+            label5.Size = new Size(149, 27);
+            label5.TabIndex = 5;
+            label5.Text = "Số Điện Thoại";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(445, 212);
+            label6.Name = "label6";
+            label6.Size = new Size(86, 27);
+            label6.TabIndex = 6;
+            label6.Text = "Địa Chỉ";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(767, 212);
+            label7.Name = "label7";
+            label7.Size = new Size(68, 27);
+            label7.TabIndex = 7;
+            label7.Text = "Email";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(46, 217);
+            label8.Name = "label8";
+            label8.Size = new Size(111, 27);
+            label8.TabIndex = 8;
+            label8.Text = "Ngày Sinh";
+            // 
+            // txtTen
+            // 
+            txtTen.Location = new Point(948, 129);
+            txtTen.Name = "txtTen";
+            txtTen.Size = new Size(150, 35);
+            txtTen.TabIndex = 10;
+            // 
+            // txtSDT
+            // 
+            txtSDT.Location = new Point(1263, 126);
+            txtSDT.Name = "txtSDT";
+            txtSDT.Size = new Size(150, 35);
+            txtSDT.TabIndex = 11;
+            // 
+            // txtDiaChi
+            // 
+            txtDiaChi.Location = new Point(563, 209);
+            txtDiaChi.Name = "txtDiaChi";
+            txtDiaChi.Size = new Size(182, 35);
+            txtDiaChi.TabIndex = 12;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(948, 204);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(150, 35);
+            txtEmail.TabIndex = 13;
+            // 
+            // mstNgay
+            // 
+            mstNgay.Location = new Point(227, 209);
+            mstNgay.Mask = "00/00/0000";
+            mstNgay.Name = "mstNgay";
+            mstNgay.Size = new Size(201, 35);
+            mstNgay.TabIndex = 14;
+            mstNgay.TypeValidationCompleted += mstNgay_TypeValidationCompleted;
+            // 
+            // dgvThongTinKhachHang
+            // 
+            dgvThongTinKhachHang.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvThongTinKhachHang.Location = new Point(12, 321);
+            dgvThongTinKhachHang.Name = "dgvThongTinKhachHang";
+            dgvThongTinKhachHang.RowHeadersWidth = 62;
+            dgvThongTinKhachHang.Size = new Size(1291, 284);
+            dgvThongTinKhachHang.TabIndex = 15;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(30, 54);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(112, 34);
+            btnThem.TabIndex = 16;
+            btnThem.Text = "Thêm";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(171, 54);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 17;
+            button2.Text = "Xóa";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // btnSua
+            // 
+            btnSua.Location = new Point(316, 54);
+            btnSua.Name = "btnSua";
+            btnSua.Size = new Size(112, 34);
+            btnSua.TabIndex = 18;
+            btnSua.Text = "Sửa";
+            btnSua.UseVisualStyleBackColor = true;
+            btnSua.Click += btnSua_Click;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(463, 54);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(112, 34);
+            btnLoad.TabIndex = 19;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // btnDong
+            // 
+            btnDong.Location = new Point(606, 54);
+            btnDong.Name = "btnDong";
+            btnDong.Size = new Size(112, 34);
+            btnDong.TabIndex = 20;
+            btnDong.Text = "Đóng";
+            btnDong.UseVisualStyleBackColor = true;
+            btnDong.Click += btnDong_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(517, 271);
+            label9.Name = "label9";
+            label9.Size = new Size(296, 27);
+            label9.TabIndex = 21;
+            label9.Text = "Hiển thị thông tin khách hàng";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(1119, 209);
+            label10.Name = "label10";
+            label10.Size = new Size(55, 27);
+            label10.TabIndex = 22;
+            label10.Text = "Phái";
+            // 
+            // cboPhai
+            // 
+            cboPhai.FormattingEnabled = true;
+            cboPhai.Location = new Point(1263, 204);
+            cboPhai.Name = "cboPhai";
+            cboPhai.Size = new Size(150, 35);
+            cboPhai.TabIndex = 23;
+            // 
+            // cboMaHang
+            // 
+            cboMaHang.FormattingEnabled = true;
+            cboMaHang.Location = new Point(563, 121);
+            cboMaHang.Name = "cboMaHang";
+            cboMaHang.Size = new Size(182, 35);
+            cboMaHang.TabIndex = 24;
+            // 
+            // CustomerForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 27F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(799, 450);
-            Name = "Form1";
-            Text = "Form1";
+            ClientSize = new Size(1436, 607);
+            Controls.Add(cboMaHang);
+            Controls.Add(cboPhai);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(btnDong);
+            Controls.Add(btnLoad);
+            Controls.Add(btnSua);
+            Controls.Add(button2);
+            Controls.Add(btnThem);
+            Controls.Add(dgvThongTinKhachHang);
+            Controls.Add(mstNgay);
+            Controls.Add(txtEmail);
+            Controls.Add(txtDiaChi);
+            Controls.Add(txtSDT);
+            Controls.Add(txtTen);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txtMaKhachHang);
+            Controls.Add(label1);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 4, 5, 4);
+            Name = "CustomerForm";
+            Text = "FormQuanLyKhachHang";
+            TopMost = true;
+            Load += CustomerForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvThongTinKhachHang).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtMaKhachHang;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox txtTen;
+        private TextBox txtSDT;
+        private TextBox txtDiaChi;
+        private TextBox txtEmail;
+        private MaskedTextBox mstNgay;
+        private DataGridView dgvThongTinKhachHang;
+        private Button btnThem;
+        private Button button2;
+        private Button btnSua;
+        private Button btnLoad;
+        private Button btnDong;
+        private Label label9;
+        private Label label10;
+        private ComboBox cboPhai;
+        private ComboBox cboMaHang;
     }
 }
--- CustomerForm/CustomerForm.cs
+++ CustomerForm/CustomerForm.cs
@@ -1,11 +1,315 @@
-using Common;
+﻿using Common;
+using System.Data;
+using System.Data.SqlClient; 
 namespace CustomerForm
 {
     public partial class CustomerForm : Form
     {
+        SqlConnection conn;
         public CustomerForm()
         {
             InitializeComponent();
+            conn = new SqlConnection("Data Source=DESKTOP-IQCO6JU\\SQLEXPRESS;Initial catalog=QL_RAP_PHIM;User ID=sa;Password=123");
+            //conn = new SqlConnection("Data Source=34.133.93.201;Initial catalog=QL_RAP_PHIM;User ID=sqlserver;Password=123456789");
+
+        }
+
+        private void CustomerForm_Load(object sender, EventArgs e)
+        {
+            if (GlobalData.Positon != 2)
+            {
+                MessageBox.Show("❌ Bạn không có quyền truy cập vào khu vực Quản lý!",
+                                "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.Close();
+                return;
+            }
+            mstNgay.ValidatingType = typeof(DateTime);
+            cboPhai.Items.Add("Nam");
+            cboPhai.Items.Add("Nữ");
+            //------------------------------//
+            LoadHangThanhVien();
+            //-----------------------------//
+
+            LoadCustomerData();
+        }
+        private void LoadCustomerData()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+
+                string query = @"
+                    SELECT 
+                        KH.MAKH,
+                        KH.MAHANG,
+                        KH.HOTENKH,
+                        KH.PHAI,
+                        KH.SDT,
+                        KH.DIACHI,
+                        KH.NGAYSINH,
+                        KH.EMAIL,
+                        H.TENHANG,
+                        H.DIEMTOITHIEU
+                    FROM KHACHHANG KH
+                    JOIN HANGTHANHVIEN H ON KH.MAHANG = H.MAHANG;
+                ";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvThongTinKhachHang.DataSource = dt;
+
+                // Ẩn cột MAHANG
+                if (dgvThongTinKhachHang.Columns["MAHANG"] != null)
+                    dgvThongTinKhachHang.Columns["MAHANG"].Visible = false;
+
+                // Đặt tên cột
+                dgvThongTinKhachHang.Columns["MAKH"].HeaderText = "Mã KH";
+                dgvThongTinKhachHang.Columns["HOTENKH"].HeaderText = "Họ tên KH";
+                dgvThongTinKhachHang.Columns["PHAI"].HeaderText = "Phái";
+                dgvThongTinKhachHang.Columns["SDT"].HeaderText = "Số điện thoại";
+                dgvThongTinKhachHang.Columns["DIACHI"].HeaderText = "Địa chỉ";
+                dgvThongTinKhachHang.Columns["NGAYSINH"].HeaderText = "Ngày sinh";
+                dgvThongTinKhachHang.Columns["EMAIL"].HeaderText = "Email";
+                dgvThongTinKhachHang.Columns["TENHANG"].HeaderText = "Hạng TV";
+                dgvThongTinKhachHang.Columns["DIEMTOITHIEU"].HeaderText = "Điểm hiện tại";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("❌ Lỗi khi tải dữ liệu: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        private void LoadHangThanhVien()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                string query = "SELECT MAHANG, TENHANG FROM HANGTHANHVIEN";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                cboMaHang.DataSource = dt;
+                cboMaHang.DisplayMember = "TENHANG";
+                cboMaHang.ValueMember = "MAHANG";
+                if (cboMaHang.Items.Count > 0)
+                    cboMaHang.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("❌ Lỗi load Hạng TV: " + ex.Message);
+            }
+            finally { conn.Close(); }
+        }
+        private void mstNgay_TypeValidationCompleted(object sender, TypeValidationEventArgs e)
+        {
+            // Nếu không nhập gì thì không báo lỗi
+            if (string.IsNullOrWhiteSpace(mstNgay.Text))
+                return;
+
+            // Nếu nhập thì phải đúng định dạng
+            if (!e.IsValidInput)
+            {
+                MessageBox.Show("Ngày sinh không hợp lệ! Vui lòng nhập theo dạng dd/MM/yyyy");
+                mstNgay.Focus();
+            }
+        }
+        bool ktr_MaKH(string ma)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                string query = "SELECT COUNT(*) FROM KHACHHANG WHERE MAKH=@MAKH";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MAKH", ma);
+                int count = (int)cmd.ExecuteScalar();
+                return count == 0; // true nếu chưa tồn tại
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kiểm tra mã KH: " + ex.Message);
+                return false;
+            }
+            finally { if (conn.State == ConnectionState.Open) conn.Close(); }
+        }
+        private void ClearForm()
+        {
+            txtMaKhachHang.Clear();
+            txtTen.Clear();
+            txtSDT.Clear();
+            txtDiaChi.Clear();
+            txtEmail.Clear();
+            cboPhai.SelectedIndex = -1;  // reset ComboBox
+            cboMaHang.SelectedIndex = -1; // reset ComboBox
+            mstNgay.Clear();
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (!ktr_MaKH(txtMaKhachHang.Text))
+            {
+                MessageBox.Show("❌ Mã khách hàng đã tồn tại!");
+                return;
+            }
+
+            if (cboMaHang.SelectedValue == null)
+            {
+                MessageBox.Show("❌ Vui lòng chọn Hạng TV!");
+                return;
+            }
+
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                string query = @"INSERT INTO KHACHHANG
+                    (MAKH, MAHANG, HOTENKH, PHAI, SDT, DIACHI, NGAYSINH, EMAIL)
+                    VALUES
+                    (@MAKH, @MAHANG, @HOTENKH, @PHAI, @SDT, @DIACHI, @NGAYSINH, @EMAIL)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MAKH", txtMaKhachHang.Text);
+                cmd.Parameters.AddWithValue("@MAHANG", cboMaHang.SelectedValue);
+                cmd.Parameters.AddWithValue("@HOTENKH", txtTen.Text);
+                cmd.Parameters.AddWithValue("@PHAI", cboPhai.Text);
+                cmd.Parameters.AddWithValue("@SDT", txtSDT.Text);
+                cmd.Parameters.AddWithValue("@DIACHI", txtDiaChi.Text);
+                cmd.Parameters.AddWithValue("@NGAYSINH", DateTime.Parse(mstNgay.Text));
+                cmd.Parameters.AddWithValue("@EMAIL", txtEmail.Text);
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("✅ Thêm khách hàng thành công!");
+                LoadCustomerData();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("❌ Lỗi khi thêm: " + ex.Message);
+            }
+            finally { conn.Close(); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dgvThongTinKhachHang.CurrentRow == null) return;
+
+            string maKH = dgvThongTinKhachHang.CurrentRow.Cells["MAKH"].Value?.ToString();
+            if (string.IsNullOrEmpty(maKH)) return;
+
+            if (MessageBox.Show($"Bạn có chắc muốn xóa khách hàng {maKH}?", "Xác nhận", MessageBoxButtons.YesNo) != DialogResult.Yes)
+                return;
+
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                string query = "DELETE FROM KHACHHANG WHERE MAKH=@MAKH";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MAKH", maKH);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("✅ Xóa khách hàng thành công!");
+                LoadCustomerData();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("❌ Lỗi khi xóa: " + ex.Message);
+            }
+            finally { conn.Close(); }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string maKH = txtMaKhachHang.Text.Trim();
+            if (string.IsNullOrEmpty(maKH))
+            {
+                MessageBox.Show("❌ Vui lòng nhập Mã khách hàng để sửa!");
+                return;
+            }
+
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                // Lấy dữ liệu cũ
+                string selectQuery = "SELECT * FROM KHACHHANG WHERE MAKH=@MAKH";
+                SqlCommand selectCmd = new SqlCommand(selectQuery, conn);
+                selectCmd.Parameters.AddWithValue("@MAKH", maKH);
+                SqlDataReader reader = selectCmd.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    MessageBox.Show("❌ Không tìm thấy khách hàng với mã này!");
+                    reader.Close();
+                    return;
+                }
+
+                reader.Read();
+                // Lấy dữ liệu cũ để so sánh
+                string oldMaHang = reader["MAHANG"].ToString();
+                string oldHoten = reader["HOTENKH"].ToString();
+                string oldPhai = reader["PHAI"].ToString();
+                string oldSDT = reader["SDT"].ToString();
+                string oldDiaChi = reader["DIACHI"].ToString();
+                DateTime oldNgaySinh = (DateTime)reader["NGAYSINH"];
+                string oldEmail = reader["EMAIL"].ToString();
+                reader.Close();
+
+                // Build câu lệnh update chỉ thay đổi những cột có giá trị mới
+                string updateQuery = @"
+            UPDATE KHACHHANG SET
+                MAHANG=@MAHANG,
+                HOTENKH=@HOTENKH,
+                PHAI=@PHAI,
+                SDT=@SDT,
+                DIACHI=@DIACHI,
+                NGAYSINH=@NGAYSINH,
+                EMAIL=@EMAIL
+            WHERE MAKH=@MAKH";
+
+                SqlCommand cmd = new SqlCommand(updateQuery, conn);
+                cmd.Parameters.AddWithValue("@MAKH", maKH);
+                cmd.Parameters.AddWithValue("@MAHANG", cboMaHang.SelectedValue ?? oldMaHang);
+                cmd.Parameters.AddWithValue("@HOTENKH", string.IsNullOrEmpty(txtTen.Text) ? oldHoten : txtTen.Text);
+                cmd.Parameters.AddWithValue("@PHAI", string.IsNullOrEmpty(cboPhai.Text) ? oldPhai : cboPhai.Text);
+                cmd.Parameters.AddWithValue("@SDT", string.IsNullOrEmpty(txtSDT.Text) ? oldSDT : txtSDT.Text);
+                cmd.Parameters.AddWithValue("@DIACHI", string.IsNullOrEmpty(txtDiaChi.Text) ? oldDiaChi : txtDiaChi.Text);
+                cmd.Parameters.AddWithValue("@NGAYSINH", string.IsNullOrEmpty(mstNgay.Text) ? oldNgaySinh : DateTime.Parse(mstNgay.Text));
+                cmd.Parameters.AddWithValue("@EMAIL", string.IsNullOrEmpty(txtEmail.Text) ? oldEmail : txtEmail.Text);
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("✅ Cập nhật khách hàng thành công!");
+                LoadCustomerData();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("❌ Lỗi khi sửa: " + ex.Message);
+            }
+            finally { conn.Close(); }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadCustomerData();
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
+
 }
